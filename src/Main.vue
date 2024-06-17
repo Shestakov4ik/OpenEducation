2c7313c72164d8ed14985283d73782575edc8f39
Egor <shestakov4ij@mail.ru>
2024-06-17 23:18:52 +0200

    1

--- src/Main.vue
+++ src/Main.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <Header v-if="auth!==true" class="header" :auth="auth" @change-page="PageChange"/>
+    <Header v-if="this.$route.name === 'cabinet'" class="header" :auth="auth" @change-page="PageChange"/>
     <router-view/>
     <Auth v-if="authOpened" @close="closeAuth" @auth="logIn"/>
-    <Footer v-if="auth!==true" class="footer"/>
+    <Footer v-if="this.$route.name === 'cabinet'" class="footer"/>
   </div>
 </template>
 
