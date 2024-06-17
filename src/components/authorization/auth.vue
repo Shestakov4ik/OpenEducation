caa1d1867a24f5ecf77299f6e97aa47e0a65400d
Egor <shestakov4ij@mail.ru>
2024-06-17 21:08:02 +0200

    1

--- src/components/authorization/auth.vue
+++ src/components/authorization/auth.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="auth-modal">
-    <div class="auth-container">
+    <div v-if="check=true" class="auth-container">
       <form ref="form" @submit.prevent="sendEmail">
         <h1 class="auth-title">Вход в кабинет</h1>
         <p class="auth-description">На указанную почту придёт СМС с кодом подтверждения</p>
@@ -16,6 +16,20 @@
           <button class="active-btn" @click="close">Закрыть</button>
         </div>
       </form>
+    </div>
+    <div v-else class="verification-modal" v-show="visible">
+      <div class="verification-container">
+        <h1 class="verification-title">Введите код</h1>
+        <p class="verification-description">Отправлено СМС на номер {{ phone }}</p>
+        <button class="verification-change" @click="changeNumber">Изменить</button>
+        <div class="pin-inputs">
+          <input v-model="pin1" class="pin-input" maxlength="1" @input="focusNext($event, 1)">
+          <input v-model="pin2" class="pin-input" maxlength="1" @input="focusNext($event, 2)">
+          <input v-model="pin3" class="pin-input" maxlength="1" @input="focusNext($event, 3)">
+          <input v-model="pin4" class="pin-input" maxlength="1" @input="submit">
+        </div>
+        <button class="resend-sms" @click="resendSMS">Отправить СМС еще раз</button>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +49,7 @@ export default {
       SERVICE_ID: 'service_h9thmof',
       TEMPLATE_ID: 'template_hnilla1',
       PUBLIC_KEY: 'wiKXGL1xjXwRni4xB',
+      check: true
     };
   },
   methods: {
@@ -48,6 +63,7 @@ export default {
                 () => console.log('SUCCESS!'),
                 (error) => console.log('FAILED...', error.text)
             );
+        this.check = false
 
 
 
@@ -156,5 +172,78 @@ export default {
   display: flex;
   flex-direction: column;
   gap: 20px;
+}
+
+.verification-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1001; /* Установим значение z-index повыше, чем у компонента Auth */
+  letter-spacing: 0.5px;
+  font-family: Gilroy-Light;
+}
+
+.verification-container {
+  background: white;
+  padding: 40px;
+  border-radius: 8px;
+  width: 500px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.verification-title {
+  font-size: 46px;
+  margin-bottom: 15px;
+  font-family: 'Gilroy-ExtraBold';
+}
+
+.verification-description {
+  font-size: 16px;
+  margin-bottom: 20px;
+  font-family: 'Gilroy-Light';
+}
+
+.pin-inputs {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.pin-input {
+  width: calc(25% - 10px);
+  height: 60px;
+  padding: 0 10px;
+  font-size: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-sizing: border-box;
+  text-align: center;
+  font-family: Gilroy-Light;
+}
+
+.resend-sms {
+  width: 100%;
+  height: 60px;
+  background-color: #78258D;
+  color: white;
+  font-size: 16px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  margin-top: 10px;
+  font-family: 'Gilroy-Light';
+}
+
+.verification-change {
+  font-size: 16px;
+  color: #78258D;
+  cursor: pointer;
+  font-family: 'Gilroy-Light';
 }
 </style>
