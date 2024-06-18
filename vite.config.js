943f31b3d2952cac16f46f94567ea42eaef3c6c8
Egor <shestakov4ij@mail.ru>
2024-06-18 13:25:00 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['components/cabinet/menu-tasks.vue']
-    }
-  },
+
   plugins: [
     vue(),
   ],
